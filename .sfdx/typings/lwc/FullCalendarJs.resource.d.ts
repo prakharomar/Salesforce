daf5e4867c4f450a4e07426ee72d4beaa6bd7b69
Prakhar Omar <prakharomar@gmail.com>
2024-04-07 01:04:23 +0530

    commi

---
+++ .sfdx/typings/lwc/FullCalendarJs.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/FullCalendarJs" {
+    var FullCalendarJs: string;
+    export default FullCalendarJs;
+}
---
+++ .sfdx/typings/lwc/SNA_prakharsales1_sf_default_cdn_RJRH3.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/SNA_prakharsales1_sf_default_cdn_RJRH3" {
+    var SNA_prakharsales1_sf_default_cdn_RJRH3: string;
+    export default SNA_prakharsales1_sf_default_cdn_RJRH3;
+}
